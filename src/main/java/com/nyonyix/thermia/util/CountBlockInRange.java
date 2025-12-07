3565d65bed173bd7a7831a6313f789cf602de2a8
Nyonyix <quontex@hotmail.com>
2025-12-08 07:45:45 +1000

    Search systems first crack

---
+++ src/main/java/com/nyonyix/thermia/util/CountBlockInRange.java
@@ -0,0 +1,9 @@
+package com.nyonyix.thermia.util;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class CountBlockInRange
+    {
+        private static final Logger LOGGER = LogUtils.getLogger();
+    }
